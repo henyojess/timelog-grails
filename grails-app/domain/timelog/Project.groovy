6728af472339518274b9af14c8920f8ae1902fb3
Jessie Evangelista <jessie@henyo.com>
2012-01-12 19:32:31 +0800

    added project data model

---
+++ grails-app/domain/timelog/Project.groovy
@@ -0,0 +1,21 @@
+package timelog
+
+class Project {
+
+    String name
+    String createdBy
+    String updatedBy
+    boolean active = true
+    Date dateCreated
+    Date lastUpdated
+    String nextRelease = '0.99.0'
+    
+    static hasMany = [ members:User]
+
+    static constraints = {
+        name blank:false
+        createdBy blank:false
+        updatedBy nullable:true,blank:false
+        nextRelease blank:false
+    }
+}
